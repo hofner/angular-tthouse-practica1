9ad9ba1efd3bb18c7585108f3b058f66270f4ca1
Juan Miguel <juan@canvasoft.com>
2020-06-14 22:01:43 -0500

    Agregando el metodo de POST para enviar un post al server mock. Usar el form on submit. Agregar comentarios dentro de la coleccion o arreglo de comentarios.

---
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -0,0 +1,31 @@
+import { Component,EventEmitter,Input , Output,ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { EntryService } from "../shared/entry.service";
+@Component(
+    {
+        selector:'app-entry-comment-form',
+        templateUrl:'entry-comment-form.component.html'
+
+    }
+)
+export class EntryCommentFormComponent{
+    name:string="";
+    comment:string="";
+    @Input() entryId:number;
+    @ViewChild('commentForm') commentForm:NgForm;
+    @Output() onCommentAdded=new EventEmitter<{name:string;comment:string;}>();
+    constructor(private entryService:EntryService){
+        
+    }
+    onSubmit(commentForm:NgForm){
+        let comment ={name:this.name, comment:this.comment};
+        this.entryService.addComment(this.entryId,comment).then(
+            ()=>{
+                this.onCommentAdded.emit(comment);
+                this.commentForm.resetForm();
+            }
+        );
+        
+    }
+
+}
